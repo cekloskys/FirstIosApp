8c3c51a6c1e5e7809408642b17c8fcc9ab924854
Sue Ceklosky <cekloskys@chc.edu>
2019-10-28 11:10:58 -0400

    Added table view controller logic.

--- FirstIosApp/NewViewController.swift
+++ FirstIosApp/NewViewController.swift
@@ -26,7 +26,25 @@ class NewViewController: UIViewController {
         tipLabel.text = tipToShow
     }
     
-
+    @IBAction func goToTableView(_ sender: UIButton) {        
+        performSegue(withIdentifier: "GoToTableView", sender: nil)
+    }
+    
+    // This method creates an instance of the View Controller
+    // that is being segued to
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // If we're segueing to the View Controller that
+        // has the identifier GoToTableView
+        if segue.identifier == "GoToTableView" {
+            
+            // Create an instance of that View Controller
+            // Use the underscore character because the
+            // instance is never going to be used
+            _ = segue.destination as? NewTableTableViewController
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
